387bc9a75cfc8890813b2e6be66ca8a47d3c98a5
EW-Shamp <ew_shamp@post.com>
2022-03-31 10:25:30 -0400

    Add Controller/View and Identity Scaffolding

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlogYou.Data;
+using BlogYou.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlogYou.Areas.Identity.IdentityHostingStartup))]
+namespace BlogYou.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
