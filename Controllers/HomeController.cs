d5b39e3eb9036fe7d8426508783b86d1016b8770
EW-Shamp <ew_shamp@post.com>
2022-04-03 04:12:07 -0400

    Add Beginning of UI formatting

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace BlogYou.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+       public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
