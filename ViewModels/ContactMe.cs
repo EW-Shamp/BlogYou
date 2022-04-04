fa4f7b317b41093c8d6d699c0a69e9831f381dda
EW-Shamp <ew_shamp@post.com>
2022-04-04 05:57:25 -0400

    Add Contact page formatting and Functionality

---
+++ ViewModels/ContactMe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogYou.ViewModels
+{
+    public class ContactMe
+    {
+        [Required]
+        [StringLength(80, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string Subject { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string Message { get; set; }
+    }
+}
