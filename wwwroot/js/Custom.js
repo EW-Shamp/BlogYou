fc3bd2b18c3dc6f18fde7ae39630026358713747
EW-Shamp <ew_shamp@post.com>
2022-04-09 16:00:23 -0400

    Add SweetAlert2 and error handling on add/delete tags

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -1,11 +1,21 @@
-﻿let index = 0;
+﻿
+let index = 0;
 function AddTag() {
     //Get a reference to the TagEntry input element
     var tagEntry = document.getElementById("TagEntry");
 
-    //Creat a new Select Option
-    let newOption = new Option(tagEntry.value, tagEntry.value);
-    document.getElementById("TagList").options[index++] = newOption;
+    //Uses search function to detect error state
+    let searchResult = search(tagEntry.value);
+    if (searchResult != null) {
+        //Trigger sweet alert for the error condition that is contained in searchResult
+        swalWithDarkButton.fire({
+            html: `<span class='font-weight-bolder'>${searchResult.toUpperCase()}</span?`,
+        });
+    } else {
+        //Creat a new Select Option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+    }
 
     //Clear out the TagEntry control
     tagEntry.value = "";
@@ -14,10 +24,19 @@ function AddTag() {
 
 function DeleteTag() {
     let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
+
+    if (tagList.selectedIndex == -1) {
+        swalWithDarkButton.fire({
+            html: `<span class="font-weight-bolder">CHOOSE TAG BEFORE DELETING</span>`
+        });
+        return true;
+    }
+
     while (tagCount > 0) {
-        let selectedIndex = document.getElementById("TagList").selectedIndex;
-        if (selectedIndex >= 0) {
-            document.getElementById("TagList").options[selectedIndex] = null;
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
             --tagCount;
         }
         else
@@ -44,3 +63,30 @@ function ReplaceTag(tag, index) {
     let newOption = new Option(tag, tag);
     document.getElementById("TagList").options[index] = newOption;
 }
+
+//The Search function will detect eiither an empthy or a duplicate Tag
+//and return an error string if an error is detected
+function search(str) {
+    if (str == "") {
+        return "Empty tags are not permitted";
+    }
+
+    var tagsEl = document.getElementById('TagList');
+    if (tagsEl) {
+        let options = tagsEl.options;
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value = str) {
+                return `The tag #${str} was detected as a duplicate and not permitted`;
+            }
+        }
+    }
+}
+
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-danger btn-sm note-btn-block btn-outline-dark'
+    },
+    imageUrl: '/img/oops.jpg',
+    timer: 3000,
+    buttonsStyling: false
+});
